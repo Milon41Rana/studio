74e59ea7045a49ee141b77e2ec7242fb73221a08
milon rana <ranamilon41@gmail.com>
2025-12-27 16:26:45 +0000

    Please fix the image loading error by:
    Updating next.config.js to allow

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'www.canva.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
